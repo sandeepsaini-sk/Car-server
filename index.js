e848decda278e925f6137adc64cede039d02b191
sandeepsaini-sk <sandeepsaini21082003@gmail.com>
2025-07-21 11:13:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOption = {
-  origin:"http://localhost:5173",
+  origin:"*",
   methods:  ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 };
